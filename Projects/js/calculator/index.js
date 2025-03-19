03e7b96541b75d9adec356e4d2863c57890624aa
Abhishek <shek54112@gmail.com>
2025-03-19 15:43:56 +0530

    comit

--- Projects/js/calculator/index.js
+++ Projects/js/calculator/index.js
@@ -1,22 +1,22 @@
 let currentInput = "";
 
-    function pickNumber(val) {
-      currentInput += val;
+function pickNumber(val) {
+  currentInput += val;
 
-      document.getElementById("display").value = currentInput;
-    }
+  document.getElementById("display").value = currentInput;
+}
 
-    function clearDisplay() {
-      currentInput = "";
-      document.getElementById("display").value = "";
-    }
+function clearDisplay() {
+  currentInput = "";
+  document.getElementById("display").value = "";
+}
 
-    function calculate() {
-      try {
-        currentInput = eval(currentInput).toString();
-        document.getElementById("display").value = currentInput;
-      } catch (e) {
-        document.getElementById("display").value = "Error";
-        currentInput = "";
-      }
-    }
+function calculate() {
+  try {
+    currentInput = eval(currentInput).toString();
+    document.getElementById("display").value = currentInput;
+  } catch (e) {
+    document.getElementById("display").value = "Error";
+    currentInput = "";
+  }
+}
